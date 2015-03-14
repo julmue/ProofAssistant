31c9e3c2bdc291f4944e62623fd090cf4257add4
julian mueller <jul.mue@hotmail.de>
2015-03-14 17:32:32 +0100

    first draft of main function and request record

--- Main.hs
+++ Main.hs
@@ -11,6 +11,40 @@ import PrettyPrint
 import Text.Parsec
 import Text.Parsec.String
 
+{- clank
+    command line arguments:
+    -- semantics: PC, L3, ...
+       -- default: PC
+    -- formula: "<String>"
+    what:
+    -- default: classify
+    -- classify:    tautology, neutrality, contradiction
+    -- valid:       validity check
+    -- sat:         satisfiability check
+    -- unsat:       unsatisfiability check
+    -- model:       one model
+    -- models:      all models
+    -- nf:          normal form (CNF,DNF,...)
+                    * default: CNF
+    -- help
+
+
+    probably best to put that all in a record;
+    some kind of "request objects" ... then a request parse could be implemened
+
+-}
+
+-- data Request = Request {
+--     getSemantics    :: Semantic
+--     getFormula      :: F.Formula
+--     getActions      :: [Action]
+-- }
+
+-- Semantic :: truth values and evaluation function
+
+data Semantic (Formula a)
+    deriving Show
+
 main :: IO ()
 main = undefined
 
